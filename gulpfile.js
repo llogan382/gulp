65fb7ba4187937e9775010e82b0084e7dc90391e
Unknown <llogan382@gmail.com>
2018-11-15 06:56:16 -0500

    Adds my first gulp method. Checkout comments

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,10 @@ gulp.task("hello", function() {
     //if running gulp hellp, "gulp" will print in the console       
     console.log("hello");
 })
+
+// the task is the default one, and it will run after an array, [], of tasks is run.
+// The dependecies are in the array ["hello"]. These will run before the default task. 
+// The default task is in the callback function. 
+gulp.task("default", ["hello"], function() {
+    console.log("this is the default task");
+});
